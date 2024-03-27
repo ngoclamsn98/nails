cfaa804a58394660f55b0dc90fe875a094ef11c8
ht-lambn <lambn@hybrid-technologies.vn>
2024-03-27 14:44:10 +0700

    fix: style

--- src/modules/Staff/Home/index.vue
+++ src/modules/Staff/Home/index.vue
@@ -1,14 +1,23 @@
 <template>
-  <div class="flex flex-col w-[90%] m-auto gap-y-[10px] justify-center h-full">
-    <RouterLink to="/staff/bill-order">
-      <div class="shadow-lg h-[200px] flex justify-center items-center text-[26px]">
-        Bill Order
-      </div>
-    </RouterLink>
-    <RouterLink to="/bill-order">
-      <div class="shadow-lg h-[200px] flex justify-center items-center text-[26px]">
-        Product
-      </div>
-    </RouterLink>
-  </div>
+   <Header :isBack="true" title="Home">
+    <template  v-slot:content>
+      <div class="flex flex-col w-[90%] m-auto gap-y-[10px] justify-center h-full">
+      <RouterLink to="/staff/bill-order">
+        <div class="card-shadow h-[200px] flex justify-center items-center text-[26px]">
+          Bill Order
+        </div>
+      </RouterLink>
+      <RouterLink to="/bill-order">
+        <div class="card-shadow h-[200px] flex justify-center items-center text-[26px]">
+          Product
+        </div>
+      </RouterLink>
+    </div>
+    </template>
+  </Header>
+  
 </template>
+
+<script setup>
+import Header from "@/components/Header";
+</script>
--- src/modules/Store/index.vue
+++ src/modules/Store/index.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="flex flex-col w-[90%] m-auto gap-y-[10px] justify-center h-full">
-        <div class="shadow-lg h-[70px] flex justify-center items-center text-[26px]" v-for="store in data.store" :key="store.value" @click="handleChooseStore(store.value)">
+        <div class="card-shadow h-[70px] flex justify-center items-center text-[26px]" v-for="store in data.store" :key="store.value" @click="handleChooseStore(store.value)">
            {{ store.name }}
       </div>
   </div>
