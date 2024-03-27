2f4421291a5ad43785d1a46ec7b05ed30d7b4427
ht-lambn <lambn@hybrid-technologies.vn>
2024-03-27 15:08:04 +0700

    fix: style

--- src/modules/Staff/Home/index.vue
+++ src/modules/Staff/Home/index.vue
@@ -3,12 +3,12 @@
     <template  v-slot:content>
       <div class="flex flex-col w-[90%] m-auto gap-y-[10px] justify-center h-full">
       <RouterLink to="/staff/bill-order">
-        <div class="card-shadow h-[200px] flex justify-center items-center text-[26px]">
+        <div class="shadow-[rgba(13,_38,_76,_0.19)_0px_9px_20px] rounded-sm h-[200px] flex justify-center items-center text-[26px]">
           Bill Order
         </div>
       </RouterLink>
       <RouterLink to="/bill-order">
-        <div class="card-shadow h-[200px] flex justify-center items-center text-[26px]">
+        <div class="shadow-[rgba(13,_38,_76,_0.19)_0px_9px_20px] rounded-sm h-[200px] flex justify-center items-center text-[26px]">
           Product
         </div>
       </RouterLink>
--- src/modules/Store/index.vue
+++ src/modules/Store/index.vue
@@ -1,6 +1,9 @@
 <template>
     <div class="flex flex-col w-[90%] m-auto gap-y-[10px] justify-center h-full">
-        <div class="card-shadow h-[70px] flex justify-center items-center text-[26px]" v-for="store in data.store" :key="store.value" @click="handleChooseStore(store.value)">
+        <div 
+            class="shadow-[rgba(13,_38,_76,_0.19)_0px_9px_20px] rounded-sm h-[70px] flex justify-center items-center text-[26px]" 
+            v-for="store in data.store" :key="store.value" @click="handleChooseStore(store.value)"
+        >
            {{ store.name }}
       </div>
   </div>
