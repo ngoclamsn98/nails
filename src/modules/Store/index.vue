eece49bc711a8cfe44f87c53d882b89b5978fb6c
lambnhybrid <lambn@hybrid.technologies.vn>
2024-03-28 23:31:22 +0700

    fix: camera

--- src/modules/Store/index.vue
+++ src/modules/Store/index.vue
@@ -8,10 +8,13 @@
     >
       {{ store.name }}
     </div>
+    <Camera />
   </div>
 </template>
 <script setup>
 import router from "@/routes";
+
+import Camera from "@/components/Camera_v2";
 import { STAFF } from "@/routes/path";
 import storageUtils from "@/utils/storageUtils";
 
