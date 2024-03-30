65c62ce7a7d4d000e3b044c9191dc0b9dce57569
lambnhybrid <lambn@hybrid.technologies.vn>
2024-03-30 22:22:27 +0700

    fix: qr code

--- src/components/QrCode/index.vue
+++ src/components/QrCode/index.vue
@@ -8,19 +8,36 @@
     class="absolute w-full h-full z-[112221] top-0 left-0 bg-[#d1d5db]"
     v-if="isOpen"
   >
-    <QrStream @decode="onDecode" />
+    <!-- <QrStream @decode="onDecode" /> -->
   </div>
+  <div id="qr-code-full-region"></div>
 </template>
 
 <script setup>
 import QrCodeIcon from "@/components/Icon/QrCode";
-import { QrStream } from "vue3-qr-reader";
+// import { QrStream } from "vue3-qr-reader";
 import useDisclosure from "@/hooks/useDisclosure";
+import { onMounted } from "vue";
 
 defineProps({
   isOpen: { type: Boolean, default: false },
 });
 
+const onScanSuccess = (decodedText, decodedResult) => {
+  console.log("result", decodedText, decodedResult);
+};
+
+onMounted(() => {
+  const config = {
+    fps: 10,
+    qrbox: 250,
+  };
+  const html5QrcodeScanner = new Html5QrcodeScanner(
+    "qr-code-full-region",
+    config
+  );
+  html5QrcodeScanner.render(onScanSuccess);
+});
 const emit = defineEmits(["qrData", "openQr"]);
 
 const { open, close, isOpen } = useDisclosure();
