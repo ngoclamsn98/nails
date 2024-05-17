98a9df3cb5785e1b9c866424dc232b01aa150cc2
ngoclamsn98 <ngoclamdk43@gmail.com>
2024-05-17 19:14:11 +0700

    fix: source cpde

--- vite.config.js
+++ vite.config.js
@@ -1,41 +1,13 @@
-import vue from '@vitejs/plugin-vue';
-import path from 'path';
-import { defineConfig } from 'vite';
-import { VitePWA } from 'vite-plugin-pwa';
+import vue from "@vitejs/plugin-vue";
+import path from "path";
+import { defineConfig } from "vite";
 export default defineConfig({
-  plugins: [
-    vue(), 
-    VitePWA({
-      registerType: 'autoUpdate',
-      workbox: {
-        globPatterns: ['**/*.{js,css,html,ico,png,svg}'],
-        sourcemap: true
-      },
-      includeAssets: ['vite.svg'],
-      manifest: {
-        name: 'Nails & Hair',
-        short_name: 'Nails & Hair',
-        description: 'Nails & Hair',
-        theme_color: '#FF69B4',
-        icons: [
-          {
-            src: 'nails&hair.jpg',
-            sizes: '192x192',
-            type: 'image/jpg'
-          },
-          {
-            src: 'nails&hair.jpg',
-            sizes: '512x512',
-            type: 'image/jpg'
-          }
-        ]
-      }
-    })],
+  plugins: [vue()],
   resolve: {
     alias: {
-      'vue$': 'vue/dist/vue.esm.js',
-      '@': path.resolve(__dirname, 'src'),
+      vue$: "vue/dist/vue.esm.js",
+      "@": path.resolve(__dirname, "src"),
     },
-    extensions: ['*', '.js', '.vue', '.json', '.ts']
+    extensions: ["*", ".js", ".vue", ".json", ".ts"],
   },
 });
